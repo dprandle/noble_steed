7b14fbd12699aed5cce0eac67228ca133d79111a
dprandle <dprandle@alaska.edu>
2019-11-23 18:03:41 -0900

    Testing pybind11 out

--- src/graphics/system.cpp
+++ src/graphics/system.cpp
@@ -1,6 +1,7 @@
 #include <noble_steed/core/system.h>
 #include <noble_steed/dbg.h>
 #include <rttr/registration>
+#include <pybind11/pybind11.h>
 
 namespace noble_steed
 {
@@ -32,6 +33,16 @@ RTTR_REGISTRATION
         .constructor<>()
         .method("log_internal", &System::log_internal, registration::public_access)
         .property("internal", &System::get_internal, &System::set_internal);
+}
+
+PYBIND11_MODULE(Noble_Steed,m)
+{
+    namespace py = pybind11;
+    py::class_<System>(m,"System")
+        .def(py::init<>())
+        .def("set_internal", &System::set_internal)
+        .def("get_internal", &System::get_internal)
+        .def("log_internal", &System::log_internal);
 }
 
 } // namespace noble_steed
